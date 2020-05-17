8655b4fbd9b633396ab71767efb4f4446a7e5741
ys9617 <ys9617@daum.net>
2020-05-18 01:38:59 +0900

    update result
    1. update content&style image
    2. add content&style reconstruction

--- README.md
+++ README.md
@@ -1,87 +1,139 @@
-[image1]: ./img/result1.PNG "Style Transfer Result"
-[image2]: ./img/VGG19_VGGCNN.PNG "VGG19"
+[image1]: ./img/content_style_reconstruction.PNG "CSR"
 
 # Image Style Transfer Using Convolutional Neural Networks
 
 Changing image style by using CNN features.
 
-![Style Transfer Result][image1]
+<figure>
+    <img src="./img/result2.PNG"/>
+    <figcaption> Content, Style and Style Transfered Image. The content image is HS J's wedding snap photo in JEJU island(2020).<br/> The style image is Egon Schiele's Self-Portrait with Physails(1912).
+    <br/> The style transfered image made by using 'A Neural Algorithm of Artistic Style'.</figcaption>
+</figure>
+
 
 ## Overview
 
-1. Load content and style images
-2. Extracting content and style features by using VGG19 layers
-3. Updating the target and calculating losses
-4. Display the target image
+1. Content and Style Reconstruction by using CNN
+2. Style Transfer
+3. Reference
 
+## Content and Style Reconstruction by using CNN
 
-## Load Content and Style Image 
+19-layer VGG network is used for representing content and style.
 
-<figure>
-    <img src="./img/content2.jpg" height="300"/>
-    <img src="./img/style1.jpg" height="300"/>
-    <figcaption> Content & Style Image</figcaption>
-</figure>
+#### CNN architecture (VGG19)
 
 ```python
-def load_image(img_path, max_size=400, shape=None):
-    """Load image (max size <= 400)"""
-    image = Image.open(img_path).convert('RGB')
-    
-    if max(image.size) > max_size:
-        size = max_size
-    else:
-        size = max(image.size)
-    
-    if shape is not None:
-        size = shape
+net = models.vgg19(pretrained=True).features
+```
+
+#### Content reconstruction
+
+```python
+contents_recon_layers = {'2':'conv1_2','7':'conv2_2','12':'conv3_2',
+                         '21':'conv4_2','30':'conv5_2'}
+
+content_features = get_features(content, net, contents_recon_layers)
+
+for layer in contents_recon_layers:
+    print('layer recon : ', contents_recon_layers[layer])
+    content_recon = content.clone()
+    content_recon[content_recon!=0] = 0
+    content_recon = content_recon.requires_grad_(True).to(device)
+    steps = 4000
+
+    optimizer = optim.Adam([content_recon], lr=0.001)
+
+    for i in range(1, steps+1):
+        content_recon_features = get_features(content_recon, net, contents_recon_layers)
+        content_loss = torch.mean((content_recon_features[contents_recon_layers[layer]] - content_features[contents_recon_layers[layer]])**2);
         
-    in_transform = transforms.Compose([
-                        transforms.Resize(size),
-                        transforms.ToTensor(),
-                        transforms.Normalize((0.485, 0.456, 0.406), 
-                                             (0.229, 0.224, 0.225))])
+        optimizer.zero_grad()
+        content_loss.backward()
+        optimizer.step()
 
-    # discard alpha channel
-    image = in_transform(image)[:3,:,:].unsqueeze(0)
-    
-    return image
+    print('content recon loss: ', content_loss.item())
+    plt.figure(figsize=(15, 15))
+    plt.imshow(image_convert(content_recon))
+    plt.show()
 ```
 
-## Extracting content and style features by using VGG19 layers
-
-#### VGG19 
-
-![VGG19][image2]
-
-
-#### Calculate Gram Matrix
+#### Style reconstruction
 
 ```python
-def gram_matrix(tensor):
-    """ Calculate the Gram Matrix of a given tensor"""
-    # get the batch_size, depth, height, and width of the Tensor
-    _, d, h, w = tensor.size()
-    
-    # calculate the gram matrix
-    tensor = tensor.view(d, h * w)
-    gram = torch.mm(tensor, tensor.t())
-    
-    return gram
+style_recon_layers = ['conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv5_1']
+style_recon_layers_arr = {'0': 'conv1_1', '5': 'conv2_1', '10': 'conv3_1','19': 'conv4_1', '28': 'conv5_1'}
+style_recon_grams = {layer: gram_matrix(style_features[layer]) for layer in style_features}
+
+for l in range(0, len(style_recon_layers)):
+    print('layer recon : ', style_recon_layers[l])
+    style_recon = style.clone()
+    style_recon[style_recon!=0] = 0
+    style_recon = style_recon.requires_grad_(True).to(device)
+    steps = 4000
+
+    optimizer = optim.Adam([style_recon], lr=0.001)
+
+    for i in range(1, steps+1):
+        style_recon_features = get_features(style_recon, net, style_recon_layers_arr)
+        style_total_loss = 0;
+
+        for c in range(0, l+1):
+            style_recon_feature = style_recon_features[style_recon_layers[c]]
+            _, d, h, w = style_recon_feature.shape
+
+            style_recon_gram = gram_matrix(style_recon_feature)
+            style_loss = torch.mean((style_recon_gram - style_recon_grams[style_recon_layers[c]])**2)
+            style_total_loss += style_loss / (d * h * w)
+
+        optimizer.zero_grad()
+        style_total_loss.backward()
+        optimizer.step()
+
+    print('style recon loss: ', style_total_loss.item())
+    plt.figure(figsize=(15, 15))
+    plt.imshow(image_convert(style_recon))
+    plt.show()
 ```
 
-## Updating the target and calculating losses
+#### Content and Style reconstruction result
+
+![Content & Style Reconstruction][image1]
+
+## Style Transfer
+
+```python
+content_weight = 1 # alpha
+style_weight = 1e3 # beta
+
+for i in range(1, steps+1):
+    target_features = get_features(target, net, layers)
+    content_loss = torch.mean((target_features['conv4_2'] - content_features['conv4_2'])**2)
+
+    style_loss = 0
+
+    for layer in style_weights:
+        _, d, h, w = target_features[layer].shape
+
+        target_gram = gram_matrix(target_features[layer])
+        layer_style_loss = style_weights[layer] * torch.mean((target_gram - style_grams[layer])**2)
+        style_loss += layer_style_loss / (d * h * w)
+
+    # total loss
+    total_loss = content_weight * content_loss + style_weight * style_loss
+
+    optimizer.zero_grad()
+    total_loss.backward()
+    optimizer.step()
+
+    if i % show_every == 0:
+        print('Total loss: ', total_loss.item())
+        plt.figure(figsize=(15, 15))
+        plt.imshow(image_convert(target))
+        plt.show()
+```
 
 
+## Reference
 
-
-## Display the target image
-
-
-
-
-
-
-
-
-
+* [Image Style Transfer Using Convolutional Neural Networks](https://www.cv-foundation.org/openaccess/content_cvpr_2016/papers/Gatys_Image_Style_Transfer_CVPR_2016_paper.pdf) by Leon A. Gatys et al
--- src/style_transfer.py
+++ src/style_transfer.py
@@ -1,175 +1,199 @@
-from PIL import Image
-from io import BytesIO
+#%%
+# style transfer
 import matplotlib.pyplot as plt
 import numpy as np
-
 import torch
 import torch.optim as optim
-import requests
+
 from torchvision import transforms, models
+from PIL import Image
 
-
-def load_image(img_path, max_size=400, shape=None):
-    """Load image (max size <= 400)"""
-    image = Image.open(img_path).convert('RGB')
+def load_image(img_path):
+    img = Image.open(img_path).convert('RGB')
     
-    if max(image.size) > max_size:
-        size = max_size
-    else:
-        size = max(image.size)
-    
-    if shape is not None:
-        size = shape
-        
     in_transform = transforms.Compose([
-                        transforms.Resize(size),
+                        transforms.Resize(224),
                         transforms.ToTensor(),
                         transforms.Normalize((0.485, 0.456, 0.406), 
                                              (0.229, 0.224, 0.225))])
 
-    image = in_transform(image)[:3,:,:].unsqueeze(0)
-    
-    return image
+    img = in_transform(img).unsqueeze(0)
 
-def im_convert(tensor):
-    """ Display a tensor as an image. """
-    image = tensor.to("cpu").clone().detach()
-    image = image.numpy().squeeze()
-    image = image.transpose(1,2,0)
-    image = image * np.array((0.229, 0.224, 0.225)) + np.array((0.485, 0.456, 0.406))
-    image = image.clip(0, 1)
+    return img
 
-    return image
+def image_convert(tensor):
+    img = tensor.to("cpu").clone().detach()
+    img = img.numpy().squeeze()
+    img = img.transpose(1,2,0)
+    img = img * np.array((0.229, 0.224, 0.225)) + np.array((0.485, 0.456, 0.406))
+    img = img.clip(0, 1)
 
-def get_features(image, model, layers=None):
-    if layers is None:
-        layers = {'0': 'conv1_1',
-                  '5': 'conv2_1', 
-                  '10': 'conv3_1', 
-                  '19': 'conv4_1',
-                  '21': 'conv4_2',  ## content representation
-                  '28': 'conv5_1'}
-        
+    return img
+
+def get_features(img, net, layers):
     features = {}
-    x = image
+    x = img
 
-    for name, layer in model._modules.items():
+    for name, layer in net._modules.items():
         x = layer(x)
+
         if name in layers:
             features[layers[name]] = x
-            
+
     return features
 
 def gram_matrix(tensor):
-    """ Calculate the Gram Matrix of a given tensor"""
-    # get the batch_size, depth, height, and width of the Tensor
     _, d, h, w = tensor.size()
     
-    # calculate the gram matrix
     tensor = tensor.view(d, h * w)
     gram = torch.mm(tensor, tensor.t())
     
     return gram
 
 
-
-
-vgg = models.vgg19(pretrained=True).features
-
-for param in vgg.parameters():
-    param.requires_grad_(False)
-
+# set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-vgg.to(device)
+# load VGG19 features
+net = models.vgg19(pretrained=True).features
 
-print(vgg)
+for param in net.parameters():
+    param.requires_grad_(False)
 
+net.to(device)
 
-# load content and style image
-content = load_image('img/content2.jpg').to(device)
-style = load_image('img/style1.jpg', shape=content.shape[-2:]).to(device)
+content = load_image("img/hs2.jpg").to(device)
+style = load_image("img/egon1.jpeg").to(device)
 
+# conv1_1, conv2-1, conv3_1, conv4_1 and conv5_1 : style
+# conv4_2 : content
+layers = {'0': 'conv1_1', '5': 'conv2_1', '10': 'conv3_1', 
+          '19': 'conv4_1', '21': 'conv4_2', '28': 'conv5_1'}
 
-# get content and style features
-content_features = get_features(content, vgg)
-style_features = get_features(style, vgg)
+content_features = get_features(content, net, layers)
+style_features = get_features(style, net, layers)
 
-
-# calculate the gram matrices for each layer of our style representation
 style_grams = {layer: gram_matrix(style_features[layer]) for layer in style_features}
 
-# create a third "target" image
 target = content.clone().requires_grad_(True).to(device)
 
+style_weights = {'conv1_1': 1.0, 'conv2_1': 0.6, 'conv3_1': 0.4,
+                 'conv4_1': 0.3, 'conv5_1': 0.1}
 
-# weights for each style layer
-style_weights = {'conv1_1': 1.0,
-                 'conv2_1': 0.6,
-                 'conv3_1': 0.4,
-                 'conv4_1': 0.3,
-                 'conv5_1': 0.1}
+content_weight = 1
+style_weight = 1e3
 
-content_weight = 1  # alpha
-style_weight = 1e6  # beta
-
-show_every = 400
+show_every = 500
 
 optimizer = optim.Adam([target], lr=0.003)
-steps = 1500  # decide how many iterations to update your image (5000)
+steps = 5000
 
-for ii in range(1, steps+1):
-    # get the features from your target image
-    target_features = get_features(target, vgg)
-    
-    # the content loss
+
+#%% training
+
+for i in range(1, steps+1):
+    target_features = get_features(target, net, layers)
     content_loss = torch.mean((target_features['conv4_2'] - content_features['conv4_2'])**2)
-    
-    # the style loss
-    # initialize the style loss to 0
+
     style_loss = 0
-    # then add to it for each layer's gram matrix loss
+
     for layer in style_weights:
-        # get the "target" style representation for the layer
-        target_feature = target_features[layer]
-        target_gram = gram_matrix(target_feature)
-        _, d, h, w = target_feature.shape
-        # get the "style" style representation
-        style_gram = style_grams[layer]
-        # the style loss for one layer, weighted appropriately
-        layer_style_loss = style_weights[layer] * torch.mean((target_gram - style_gram)**2)
-        # add to the style loss
+        _, d, h, w = target_features[layer].shape
+
+        target_gram = gram_matrix(target_features[layer])
+        layer_style_loss = style_weights[layer] * torch.mean((target_gram - style_grams[layer])**2)
         style_loss += layer_style_loss / (d * h * w)
-        
-    # calculate the *total* loss
+
+    # total loss
     total_loss = content_weight * content_loss + style_weight * style_loss
-    
-    # update your target image
+
     optimizer.zero_grad()
     total_loss.backward()
     optimizer.step()
-    
-    # display intermediate images and print the loss
-    if  ii % show_every == 0:
+
+    if i % show_every == 0:
         print('Total loss: ', total_loss.item())
-        #plt.imshow(im_convert(target))
-        #plt.show()
+        plt.figure(figsize=(15, 15))
+        plt.imshow(image_convert(target))
+        plt.show()
 
+    
 
-
-
-# display the image
-plt.figure()
+plt.figure(figsize=(25, 255))
 plt.subplot(1, 3, 1)
-plt.imshow(im_convert(content))
+plt.imshow(image_convert(content))
 
 plt.subplot(1, 3, 2)
-plt.imshow(im_convert(style))
+plt.imshow(image_convert(style))
 
 plt.subplot(1, 3, 3)
-plt.imshow(im_convert(target))
+plt.imshow(image_convert(target))
 
 plt.show()
 
+#%% content reconstruction
 
+# content reconstruction
+contents_recon_layers = {'2':'conv1_2','7':'conv2_2','12':'conv3_2',
+                         '21':'conv4_2','30':'conv5_2'}
+
+content_features = get_features(content, net, contents_recon_layers)
+
+for layer in contents_recon_layers:
+    print('layer recon : ', contents_recon_layers[layer])
+    style_recon = content.clone()
+    style_recon[style_recon!=0] = 0
+    style_recon = style_recon.requires_grad_(True).to(device)
+    steps = 4000
+
+    optimizer = optim.Adam([style_recon], lr=0.001)
+
+    for i in range(1, steps+1):
+        content_recon_features = get_features(style_recon, net, contents_recon_layers)
+        content_loss = torch.mean((content_recon_features[contents_recon_layers[layer]] - content_features[contents_recon_layers[layer]])**2);
+        
+        optimizer.zero_grad()
+        content_loss.backward()
+        optimizer.step()
+
+    print('content recon loss: ', content_loss.item())
+    plt.figure(figsize=(15, 15))
+    plt.imshow(image_convert(style_recon))
+    plt.show()
+
+
+#%% style reconstruction
+style_recon_layers = ['conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv5_1']
+style_recon_layers_arr = {'0': 'conv1_1', '5': 'conv2_1', '10': 'conv3_1','19': 'conv4_1', '28': 'conv5_1'}
+style_recon_grams = {layer: gram_matrix(style_features[layer]) for layer in style_features}
+
+for l in range(0, len(style_recon_layers)):
+    print('layer recon : ', style_recon_layers[l])
+    style_recon = style.clone()
+    style_recon[style_recon!=0] = 0
+    style_recon = style_recon.requires_grad_(True).to(device)
+    steps = 4000
+
+    optimizer = optim.Adam([style_recon], lr=0.001)
+
+    for i in range(1, steps+1):
+        style_recon_features = get_features(style_recon, net, style_recon_layers_arr)
+        style_total_loss = 0;
+
+        for c in range(0, l+1):
+            style_recon_feature = style_recon_features[style_recon_layers[c]]
+            _, d, h, w = style_recon_feature.shape
+
+            style_recon_gram = gram_matrix(style_recon_feature)
+            style_loss = torch.mean((style_recon_gram - style_recon_grams[style_recon_layers[c]])**2)
+            style_total_loss += style_loss / (d * h * w)
+
+        optimizer.zero_grad()
+        style_total_loss.backward()
+        optimizer.step()
+
+    print('style recon loss: ', style_total_loss.item())
+    plt.figure(figsize=(15, 15))
+    plt.imshow(image_convert(style_recon))
+    plt.show()
 
